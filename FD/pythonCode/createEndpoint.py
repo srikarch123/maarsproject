510eb61df7b025e71421d5f9893a085d8260511a
Pavan Guggilla <pguggilla2@unl.edu@macbook-air.unl.edu>
2023-10-31 11:46:27 -0500

    changed the structure of the project

--- FD/pythonCode/createEndpoint.py
+++ FD/pythonCode/createEndpoint.py
@@ -6,7 +6,7 @@ CORS(app)
 
 # Create a data structure to store the uploaded coordinates
 coordinates = []
-
+singleCoordinate=[{"lat":40.832532457322095,"lon":-96.66922804017223}]
 
 @app.route('/upload_coordinates', methods=['POST'])
 def upload_coordinates():
@@ -15,6 +15,8 @@ def upload_coordinates():
         latitude = data['lat']
         longitude = data['lon']
         coordinates.append({'lat': latitude, 'lon': longitude})
+        singleCoordinate.clear()
+        singleCoordinate.append({'lat': latitude, 'lon': longitude})
         return jsonify({'message': 'Coordinates uploaded successfully'})
     else:
         return jsonify({'error': 'Invalid data format'}), 400
@@ -24,9 +26,12 @@ def get_coordinates():
     data = {'coordinates': coordinates}
     return jsonify(data)
 
-# Define a custom function to get coordinates without making an HTTP request
-def custom_get_coordinates():
-    return coordinates
+
+@app.route('/get_single_coordinate', methods=['GET'])
+def get_single_coordinate():
+    data1 = {'coordinates': singleCoordinate}
+    return jsonify(data1)
+
 
 if __name__ == '__main__':
-    app.run(debug=True,port=4733)
+    app.run(debug=True,port=4753)
